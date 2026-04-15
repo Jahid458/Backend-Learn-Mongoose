dccb6ad6b149b0162f8925e21c448d69c3e9eed4
Jahid458 <Jahidul898@outlook.com>
2026-04-15 22:08:59 +0600

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,7 @@ import { DB_NAME } from "../constant.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstant = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
+    const connectionInstant = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log(`\n MongoDB Connect!!! DB HOST: ${connectionInstant.connection.host}`);
   } catch (error) {
     console.log("MONGODB connection Error", error);
